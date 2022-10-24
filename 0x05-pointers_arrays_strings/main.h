7f0d26e215a0d6dbbd80990b286e2fd50c85106a
arstenik <ibechristopher160@gmail.com>
2022-10-24 21:37:32 +0000

    fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -12,3 +16,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif
